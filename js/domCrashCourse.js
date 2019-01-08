545ece9c2049c70ac85651da57f6799d6a4574bc
NZY\NZY <mandreicosmin@yahoo.com>
2019-01-08 20:48:31 +0200

    day080119

--- js/domCrashCourse.js
+++ js/domCrashCourse.js
@@ -2,3 +2,13 @@
 
 let burger = document.getElementById('menu');
 burger.innerHTML = '<h3>Burger Menu</h3>';
+
+let val = document.getElementById('value').innerText
+
+let putIn = document.getElementById('inp');
+putIn.value = val + (' (done)');
+
+// change name dinamicaly via PROMPT
+let userName = prompt('What is your name?');
+let welcome = document.getElementById('welcome');
+welcome.innerText = 'Hello ' + userName;
