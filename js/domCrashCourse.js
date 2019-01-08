da189f747674fccc9abf481af3665e0c8f352ff4
NZY\NZY <mandreicosmin@yahoo.com>
2019-01-08 23:18:48 +0200

    3 sets of conditional - ifelse, switch and ternary

--- js/domCrashCourse.js
+++ js/domCrashCourse.js
@@ -1,14 +1,14 @@
-// git checkout
+// // git checkout
 
-let burger = document.getElementById('menu');
-burger.innerHTML = '<h3>Burger Menu</h3>';
+// let burger = document.getElementById('menu');
+// burger.innerHTML = '<h3>Burger Menu</h3>';
 
-let val = document.getElementById('value').innerText
+// let val = document.getElementById('value').innerText
 
-let putIn = document.getElementById('inp');
-putIn.value = val + (' (done)');
+// let putIn = document.getElementById('inp');
+// putIn.value = val + (' (done)');
 
-// change name dinamicaly via PROMPT
-// let userName = prompt('What is your name?');
-// let welcome = document.getElementById('welcome');
-// welcome.innerText = 'Hello ' + userName;
+// // change name dinamicaly via PROMPT
+// // let userName = prompt('What is your name?');
+// // let welcome = document.getElementById('welcome');
+// // welcome.innerText = 'Hello ' + userName;
